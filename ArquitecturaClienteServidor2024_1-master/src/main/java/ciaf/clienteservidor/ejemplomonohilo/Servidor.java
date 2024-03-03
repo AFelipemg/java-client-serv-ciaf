fee54642697c6e11bd160cebd6a28a1d96ea7452
Andres Marin G <af.marin17@ciaf.edu.co>
2024-03-03 05:16:44 -0500

    change_from_single thread to multithread with pool

--- ArquitecturaClienteServidor2024_1-master/src/main/java/ciaf/clienteservidor/ejemplomonohilo/Servidor.java
+++ ArquitecturaClienteServidor2024_1-master/src/main/java/ciaf/clienteservidor/ejemplomonohilo/Servidor.java
@@ -2,9 +2,13 @@ package ciaf.clienteservidor.ejemplomonohilo;
 
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
 
 public class Servidor {
     public static void main(String[] args) {
+        // Crea un pool de 10 hilos
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
         try {
             ServerSocket serverSocket = new ServerSocket(5000); // Puerto del servidor
 
@@ -12,12 +16,19 @@ public class Servidor {
                 Socket socket = serverSocket.accept(); // Espera por la conexión del cliente
                 System.out.println("Cliente conectado.");
 
-                ClienteHandler clienteHandler = new ClienteHandler(socket);
-                Thread clienteThread = new Thread(clienteHandler); // Inicia un nuevo hilo para el cliente
-                clienteThread.start();
+                //monohilo
+                //ClienteHandler clienteHandler = new ClienteHandler(socket);
+                //Thread clienteThread = new Thread(clienteHandler); // Inicia un nuevo hilo para el cliente
+                //clienteThread.start();
+
+
+                // Usa el pool de hilos para manejar la conexión del cliente
+                executor.submit(new ClienteHandler(socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            executor.shutdown();
         }
     }
 }
